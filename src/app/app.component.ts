62ad58b4fb2bbd3980efdfb835b1690e58fcaa01
harry <harry74323@gmail.com>
2018-02-25 12:26:44 +1100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,53 @@ import { Customer } from './customer';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  customer: Customer = {
-    id: 1,
-    name: 'Alex Smith',
-    address: {
-      street: '123 Main Street',
-      city: 'Anytown',
-      state: 'California',
-      region: 'West'
-    }
-  };
+
+  customers: Customer[] = [
+    {
+      id: 1,
+      name: 'Alex Smith',
+      address: {
+        street: '123 Main Street',
+        city: 'Anytown',
+        state: 'California',
+        region: 'West'
+      }
+    },
+    {
+      id: 2,
+      name: 'Pierre Pasmal',
+      address: {
+        street: '456 Rue de Main',
+        city: 'Quebec City',
+        state: 'Quebec',
+        region: 'East'
+      }
+    },
+    {
+      id: 3,
+      name: 'Margarita Nadie',
+      address: {
+        street: '789 Calle Principal',
+        city: 'Guadalajara',
+        state: 'Jalisco',
+        region: 'South'
+      }
+    },
+    {
+      id: 4,
+      name: 'Katie O\'Leary',
+      address: {
+        street: '137 DeKoven Street',
+        city: 'Chicago',
+        state: 'Illinois',
+        region: 'Midwest'
+      }
+    },
+  ];
+
+  customer: Customer;
+
+  hideAddress = false;
+
+  regions = ['East', 'Midwest', 'North', 'South', 'West'];
 }
