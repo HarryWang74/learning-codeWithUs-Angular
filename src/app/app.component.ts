b265b4f0a4fa1e98b56ff58cdf8743c953e2a6ef
Harry <harry74323@gmail.com>
2018-02-26 17:03:55 +1100

    create customerList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Customer } from './customer';
 
 @Component({
   selector: 'app-root',
@@ -8,52 +7,4 @@ import { Customer } from './customer';
 })
 export class AppComponent {
 
-  customers: Customer[] = [
-    {
-      id: 1,
-      name: 'Alex Smith',
-      address: {
-        street: '123 Main Street',
-        city: 'Anytown',
-        state: 'California',
-        region: 'West'
-      }
-    },
-    {
-      id: 2,
-      name: 'Pierre Pasmal',
-      address: {
-        street: '456 Rue de Main',
-        city: 'Quebec City',
-        state: 'Quebec',
-        region: 'East'
-      }
-    },
-    {
-      id: 3,
-      name: 'Margarita Nadie',
-      address: {
-        street: '789 Calle Principal',
-        city: 'Guadalajara',
-        state: 'Jalisco',
-        region: 'South'
-      }
-    },
-    {
-      id: 4,
-      name: 'Katie O\'Leary',
-      address: {
-        street: '137 DeKoven Street',
-        city: 'Chicago',
-        state: 'Illinois',
-        region: 'Midwest'
-      }
-    },
-  ];
-
-  customer: Customer;
-
-  hideAddress = false;
-
-  regions = ['East', 'Midwest', 'North', 'South', 'West'];
 }
